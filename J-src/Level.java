f5a7fbbc4230a9259910adce1a42014eca5b7be0
Jiisgone <97993393+Jiisgone@users.noreply.github.com>
2022-01-24 16:51:47 -0500

    Update Level.java
    Removed debug

--- J-src/Level.java
+++ J-src/Level.java
@@ -1,47 +1,119 @@
-/*
- * Date Created: 18/01/22
- * File Owner: Julian Sanchez
- * Date Last Updated:
- * Description:
- */
-public class Level {
 
-	//Properties
-	int level;
-	int chancesRemaining;
-	SecretWord hiddenWord;
-	boolean debugOn;
+import java.util.ArrayList;
+
+public class Level implements Cloneable {
+
+	// Properties
 	
-	//Construct
-	public Level(int lvl, int chances, SecretWord sWord, boolean debug) {
-		level = lvl;
-		chancesRemaining = chances;
-		hiddenWord = sWord;
-		debugOn = debug;
+	private int levelNumber;
+	private int chancesRemaining;
+	private SecretWord secretWord;
+	private boolean debugOn = false;
+	
+	// Constructor
+	
+	public Level(int levelNumber, int chancesRemaining, SecretWord secretWord) {
+		this.levelNumber = levelNumber;
+		this.chancesRemaining = chancesRemaining;
+		this.secretWord = secretWord;
 	}
 	
-	//methods
+	
+	
+	// Getters / Setters
+	
+	public int getLevelNumber() {
+		return levelNumber;
+	}
+
+
+
+	public void setLevelNumber(int levelNumber) {
+		this.levelNumber = levelNumber;
+	}
+
+
+
+	public int getChancesRemaining() {
+		return chancesRemaining;
+	}
+
+
+
+	public void setChancesRemaining(int chancesRemaining) {
+		this.chancesRemaining = chancesRemaining;
+	}
+
+
+
+	public SecretWord getSecretWord() {
+		return secretWord;
+	}
+
+
+
+	public void setSecretWord(SecretWord secretWord) {
+		this.secretWord = secretWord;
+	}
+
+
+
+	public boolean isDebugOn() {
+		return debugOn;
+	}
+
+
+
+	public void setDebugOn(boolean debugOn) {
+		this.debugOn = debugOn;
+	}
+
+
+	// Methods
+
 	public String toString() {
 		
-		String hideWord = hiddenWord;
-		hideWord - hideWord.replaceAll(hideWord, hideWord)
+		if(debugOn) {
+			return "Level: " + levelNumber + ", Chances Remaining: " 
+			+ chancesRemaining + ", Secret Word: " + secretWord.getWordState() 
+			+ ", Actual Word: " + secretWord.getActualWord();
+		}
 		
-		System.out.println("Level " + level);
-		System.out.println("Remaining number of guesses: " + chancesRemaining);
-		//print out the new 
-		
+		return "Level: " + levelNumber + ", Chances Remaining: " 
+				+ chancesRemaining + ", Secret Word: " + secretWord.getWordState();
 	}
 	
-	public void checkGuess(char guessedLetter) {
+	public boolean checkGuesses(char guessedLetter) {
+		
+		char capitalGuessedLetter = Character.toUpperCase(guessedLetter);
+
+		if(secretWord.containsLetter(capitalGuessedLetter)) {
+			// TODO: update the state of the secretWord accordingly
+			ArrayList<Character> correctGuesses = secretWord.getCorrectGuesses();
+			if(!correctGuesses.contains(capitalGuessedLetter)) {
+				correctGuesses.add(capitalGuessedLetter);
+				secretWord.setCorrectGuesses(correctGuesses);
+				secretWord.calculateWordState(capitalGuessedLetter);
+			}
+			else {
+				System.out.println("You already guessed " + capitalGuessedLetter + " before");
+			}
+			
+			return true;
+		}
+		else {
+			chancesRemaining--;
+			return false;
+		}
 		
 	}
 	
 	public boolean isWordGuessed() {
-		
-		boolean answered;
-		
-		hiddenWord == //variable with the user's answer
-		
-		return answered;
+		return !secretWord.hasLettersRemaning();
 	}
+	
+	protected Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+	
 }
